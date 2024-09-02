4dd66b7ec09d8ff664f2193a7b1455b5e975fdc1
alphacoder-mp3 <shindeakshay223@gmail.com>
2024-09-02 19:55:57 +0530

    Update socials data

--- src/data/socialsData.js
+++ src/data/socialsData.js
@@ -1,14 +1,14 @@
 export const socialsData = {
-    github: 'https://github.com/',
-    facebook: 'https://www.facebook.com/dolly.kumar.1291',
-    linkedIn: 'https://www.linkedin.com/in/kdolly/',
-    instagram: 'https://www.instagram.com/tapasvini_/',
-    codepen: 'https://codepen.io/',
-    twitter: 'https://twitter.com/',
-    reddit: 'https://www.reddit.com/user/',
-    blogger: 'https://www.blogger.com/',
-    medium: 'https://medium.com/@',
-    stackOverflow: 'https://stackoverflow.com/users/',
-    gitlab: 'https://gitlab.com/',
-    youtube: 'https://youtube.com/'
-}
+  github: 'https://github.com/',
+  facebook: 'https://www.facebook.com/dolly.kumar.1291',
+  linkedIn: 'https://www.linkedin.com/in/kdolly/',
+  instagram: 'https://www.instagram.com/dolly.kumar_/',
+  codepen: 'https://codepen.io/',
+  twitter: 'https://twitter.com/',
+  reddit: 'https://www.reddit.com/user/',
+  blogger: 'https://www.blogger.com/',
+  medium: 'https://medium.com/@',
+  stackOverflow: 'https://stackoverflow.com/users/',
+  gitlab: 'https://gitlab.com/',
+  youtube: 'https://youtube.com/',
+};
